e79495d969012a33c0e1cb5512e566ee3f48b0fa
rslivepark <rslivepark@gmail.com>
2024-06-11 21:20:37 +0900

    코드 리팩토링

--- src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
 import { Alert } from 'react-bootstrap';
-import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import './PopularMovieSlide.style.css';
-import MovieCard from '../MovieCard/MovieCard';
+import MovieSlider from '../../../../common/MovieSlider/MovieSlider';
+import { responsive } from '../../../../constants/response';
 
 const PopularMovieSlide = () => {
-  // const titles = ['Popular Movies', 'Top rated Movies', 'Upcoming Movies'];
+  const titles = ['Popular Movies', 'Top rated Movies', 'Upcoming Movies'];
 
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
 
@@ -19,37 +19,18 @@ const PopularMovieSlide = () => {
     return <Alert variant='danger'>{error.message}</Alert>;
   }
 
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
-
   console.log('PopularMovieSlide', data);
 
   return (
     <div>
-      <h3>Popular Movies</h3>
-      <Carousel
-        infinite={true}
-        centerMode={true}
-        itemClass='movie-slider p-1'
-        containerClass='carousel-container'
-        responsive={responsive}
-      >
-        {data.results.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </Carousel>
+      {titles.map((item, id) => (
+        <MovieSlider
+          title={item}
+          movies={data.results}
+          responsive={responsive}
+          key={id}
+        />
+      ))}
     </div>
   );
 };
